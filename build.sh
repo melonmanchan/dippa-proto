d03e2912e5114e1c47386202b92398cac5abb750
Matti Jokitulppo <melonmanchan@gmail.com>
2018-07-06 14:22:21 +0300

    Add types

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 set -e
 protoc types.proto --go_out=build/go --python_out=build/python
+
+cp build/python/types_pb2.py ../dippa-nlp
+cp build/go/types.pb.go ../dippa-facerec/src/types
+cp build/go/types.pb.go ../dippa-publish/src/types
